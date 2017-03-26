f83a3d093ca6710d14d62858f51435334dc803b4
NikolaCarovic <nikola.carovic@yahoo.com>
2017-03-26 20:47:40 +0200

    Uvedene i redefinisane metode toString, equals i hashCode

--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -4,15 +4,15 @@ import java.util.LinkedList;
 
 public class Valuta {
 	
-	private String nazic;
+	private String naziv;
 	private String skraceniNaziv;
 	private LinkedList<Kurs> kurs;
 	
 	public String getNazic() {
-		return nazic;
+		return naziv;
 	}
 	public void setNazic(String nazic) {
-		this.nazic = nazic;
+		this.naziv = nazic;
 	}
 	public String getSkraceniNaziv() {
 		return skraceniNaziv;
@@ -25,5 +25,28 @@ public class Valuta {
 	}
 	public void setKurs(LinkedList<Kurs> kurs) {
 		this.kurs = kurs;
+	}
+	@Override
+	public String toString() {
+		return "Valuta [nazic=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ", kurs=" + kurs + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kurs == null) ? 0 : kurs.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if( obj instanceof Valuta){
+			Valuta v = (Valuta) obj;
+			if(v.getNazic().equals(naziv) && v.getSkraceniNaziv().equals(skraceniNaziv)){
+				return true;
+			}
+		}
+		return false;
 	}
 }
