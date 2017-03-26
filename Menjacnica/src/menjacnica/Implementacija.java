81d5e2bc94be1046cf98c88139e7df459096e9a2
NikolaCarovic <nikola.carovic@yahoo.com>
2017-03-26 21:34:59 +0200

    Uvedena klasa koja implementira interfejs

---
+++ Menjacnica/src/menjacnica/Implementacija.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import noviPaket.Interfejs;
+
+public class Implementacija implements Interfejs {
+
+	@Override
+	public void dodajKursValuteNaDan(String naziv, Kurs kurs, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursValuteZaDan(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pronadjiKursZaDan(String naziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
